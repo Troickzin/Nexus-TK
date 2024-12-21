0e1e438cdfb695e88c186325e60bfd7a31611b66
Troickzin <troickzin@gmail.com>
2024-12-20 21:55:39 -0300

    sla

--- src/Components/Overlay.jsx
+++ src/Components/Overlay.jsx
@@ -4,7 +4,7 @@ export default function Overlay(props) {
   return (
     <>
       <div className="fixed top-0 left-0 h-screen w-[32px] bg-nexus-dark-400 border-r-2 border-nexus-white-400 shadow-shadow-nexus-s10 z-[100]" />
-      <Mini_Logo TitleFix={props.TitleFix} />
+      {/* <Mini_Logo TitleFix={props.TitleFix} /> */}
     </>
   );
 }
--- src/app/[locale]/hub/page.jsx
+++ src/app/[locale]/hub/page.jsx
@@ -1,39 +1,38 @@
+import Image from "next/image";
+import TestForm from "@/Components/TestForm";
+import Profile from "@/Components/Float/Menu/Profile";
+import { signOut } from "next-auth/react";
+import DeslogarButton from "@/Components/DeslogarButton";
+import FilesRouter from "@/Components/FilesRouter";
+import Overlay from "@/Components/Overlay";
+import Background from "@/Containers/Background";
 import Float_Menu from "@/Components/Float/Menu/Float_Menu";
 import Navbar from "@/Components/Float/Navbar/Navbar";
-import Overlay from "@/Components/Overlay/Overlay";
-import Image from "next/image";
 
-export const metadata = {
-  title: "Nexus TK - HUB",
-};
-
-export default async function Hub() {
+export default async function Home() {
   return (
     <>
-      <div className="text-nexus-white-50 w-full h-full relative z-10">
-        <div className="relative top-0 left-0 w-full h-screen ">
-          <div className="w-full h-full flex items-center justify-center">
-            <div className="size-[200px] bg-gradient-to-t from-nexus-primary-color/20 border-nexus-primary-color/75 border-[5px] box-border rounded-full absolute animate-spin"></div>
-            <div className="size-[250px] from-nexus-white-50/20 border-nexus-white-50/75 border-[2px] box-border rounded-full absolute animate-newsping"></div>
-            <div className="size-[250px] rounded-full absolute grid place-items-center">
-              <Image
-                src="/images/nx.png"
-                width={150}
-                height={150}
-                alt="image"
-                className=""
-              />
-            </div>
+      <div className="w-screen h-screen overflow-hidden">
+        <div className="grid ml-[75px] mt-[160px] w-fit">
+          <p className="text-nexus-white-100 text-4xl">Bem-Vindo ao</p>
+          <p className="text-nexus-white-50 text-8xl mt-[-10px] mb-[-10px]">
+            <span className="text-nexus-primary-color">N</span>EXUS{" "}
+            <span className="text-nexus-primary-color">T</span>K
+          </p>
+          <p className="text-nexus-white-100 text-xl">
+            Um ecossistema para servidores Apocaliptico de Minecraft
+          </p>
+          <div className="flex gap-1 justify-center items-center">
+            <div className="h-[2px] w-1/2 rounded-sm bg-gradient-to-l from-nexus-white-50"></div>
+            <div className="size-[6px] rounded-[1px] bg-nexus-eft-300 rotate-45"></div>
+            <div className="h-[2px] w-1/2 rounded-sm bg-gradient-to-r from-nexus-white-50"></div>
           </div>
         </div>
-        <div
-          className="relative w-full h-screen border border-red-500"
-          id="test"
-        ></div>
+
+        <Navbar />
+        <Float_Menu />
+        <Overlay />
       </div>
-      <Navbar />
-      {/* <Float_Menu /> */}
-      <Overlay TitleFix="true" />
     </>
   );
 }
